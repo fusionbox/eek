b5d7815b5fdee13ff5002e7732dcff527999faab
Gavin Wahl <gwahl@fusionbox.com>
2011-12-12 16:17:13 -0700

    make it a package

---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+import os
+import re
+from setuptools import setup
+
+__doc__="""
+Eek is an HTTP spider that collects metadata from HTML
+"""
+
+version = '0.0.1'
+
+setup(name='Eek',
+    version=version,
+    description='eek',
+    author='Gavin Wahl',
+    author_email='gwahl@fusionbox.com',
+    long_description=__doc__,
+    packages=['eek'],
+    scripts=['eek/eek'],
+    platforms = "any",
+    license='BSD',
+)
